78827f0f71ef1ccd24aaa6ee55aebcf1f078f222
alvpia <alpia.memo@gmail.com>
2017-06-19 17:25:51 +0900

    Update def.js

--- js/def.js
+++ js/def.js
@@ -12,6 +12,6 @@ if(answer === number){
   message="0~3の数字を入力してください";
 }
 
-document.getElementById('choice').textContent = number;
+document.getElementById('choice').textContent = "aaaa";
 
 document.getElementById('choice').textContent = message;
